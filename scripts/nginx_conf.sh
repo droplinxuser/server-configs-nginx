0332056e7d531ce367e65d8ab84a0627f2da877e
droplinxuser <68084306+droplinxuser@users.noreply.github.com>
2024-04-12 14:06:34 +0800

    Update nginx_conf.sh

--- scripts/nginx_conf.sh
+++ scripts/nginx_conf.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
 cd ~
 /usr/bin/rm -rf server-configs-nginx-main main.zip*
 /usr/bin/wget -q https://github.com/droplinxuser/server-configs-nginx/archive/refs/heads/main.zip
 /usr/bin/unzip -q main.zip
 /usr/bin/rm -rf server-configs-nginx-main/{*.md,*.txt,.git*}
 /usr/bin/rsync -a server-configs-nginx-main/ /etc/nginx/
-nginx -t && service nginx reload
+/usr/sbin/nginx -t && /usr/bin/systemctl reload nginx.service
